6c7c01ef4b8936ec37b016588ba412151bee9d9e
niallstack <niallstack@outlook.com>
2017-02-16 11:59:03 +0000

    Uploaded Project
    This just has a basic UI with a couple of formulas and a temporary
    driver class

---
+++ FormulaCalc/src/com/stack/niall/Areas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stack.niall;
+
+/**
+ *
+ * @author t00174406
+ */
+public class Areas {
+    public double Triangle()
+    {
+        return 333.33;
+    }
+    public double Circle()
+    {
+        return 43434.3434;
+    }
+    public double Rectangle()
+    {
+        return 3434.343;
+    }
+}
